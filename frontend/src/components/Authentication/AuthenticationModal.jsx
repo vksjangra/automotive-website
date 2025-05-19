27a79fe7fc8600dfb906d94d2ae679182654d63a
Vikas <vikasjangraa@gmail.com>
2025-05-20 03:01:41 +0530

    Added mongodb creds

--- frontend/src/components/Authentication/AuthenticationModal.jsx
+++ frontend/src/components/Authentication/AuthenticationModal.jsx
@@ -5,7 +5,7 @@ import LoginUser from './LoginUser';
 
 Modal.setAppElement("#root");
 
-const AuthenticationModal = ( { authModalIsOpen, closeAuthModal } ) => {
+const AuthenticationModal = ({ authModalIsOpen, closeAuthModal }) => {
 
     const [view, setView] = useState(0);
 
@@ -13,26 +13,25 @@ const AuthenticationModal = ( { authModalIsOpen, closeAuthModal } ) => {
         <div>
             <Modal isOpen={authModalIsOpen} onRequestClose={closeAuthModal} contentLabel="Authentication Modal">
                 <button className='py-1 px-3 absolute right-4 border-1 border-gray-200 rounded-full hover:cursor-pointer hover:border-blue-500' onClick={closeAuthModal}>X</button>
-                {!view ? 
-                <div>
-                    <LoginUser />
-                    <div className='text-center mt-30'>
-                        <p>New User?</p>
-                        <button className='border-gray-200 px-3 py-2 bg-blue-500 rounded-lg text-white hover:bg-blue-600 cursor-pointer' onClick={()=>setView(view=>!view)}>Create Account</button>
+                {!view ?
+                    <div>
+                        <LoginUser />
+                        <div className='text-center mt-30'>
+                            <p>New User?</p>
+                            <button className='border-gray-200 px-3 py-2 bg-blue-500 rounded-lg text-white hover:bg-blue-600 cursor-pointer' onClick={() => setView(view => !view)}>Create Account</button>
+                        </div>
                     </div>
-                </div>
-                :
-                <div>
-                    <CreateUser />
-                    <div className='text-center mt-20'>
-                        <p>
-                            Already Signed Up?
-                        </p>
-                        <button className='border-gray-200 px-3 py-2 bg-blue-500 rounded-lg text-white hover:bg-blue-600 cursor-pointer' onClick={()=>setView(view=>!view)}>Login Here</button>
+                    :
+                    <div>
+                        <CreateUser />
+                        <div className='text-center mt-20'>
+                            <p>
+                                Already Signed Up?
+                            </p>
+                            <button className='border-gray-200 px-3 py-2 bg-blue-500 rounded-lg text-white hover:bg-blue-600 cursor-pointer' onClick={() => setView(view => !view)}>Login Here</button>
+                        </div>
                     </div>
-                </div>
-                    }
-
+                }
             </Modal>
         </div>
     );
